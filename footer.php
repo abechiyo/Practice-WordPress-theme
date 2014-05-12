2717b0b6c2bf12b410d40f6160954c0bab49a03f
abechiyo <masayuki.abe@kddi-web.com>
2014-05-12 11:34:50 +0900

    add wp_footer

--- footer.php
+++ footer.php
@@ -20,5 +20,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/bootstrap/js/bootstrap.min.js"></script>
+  <?php wp_footer(); ?>
   </body>
 </html>
